6e9eea64fea7f182455a8def1ceeb8c126a84e3f
neo.xu <neo@mailejifen.com>
2016-05-14 14:11:11 +0800

    重定义，增加监控聚合图形

--- nginx/nginx_status.sh
+++ nginx/nginx_status.sh
@@ -18,5 +18,14 @@ Writing(){
 Waiting(){
         wget --quiet -O - http://localhost:40080/nginx_status?auto |awk 'NR==4 {print$6}'
 }
+Accepts(){
+  		wget --quiet -O - http://localhost:40080/nginx_status?auto |awk 'NR==3 {print $1}'
+}
+Handled(){
+		wget --quiet -O - http://localhost:40080/nginx_status?auto |awk 'NR==3 {print $2}'
+}
+Requests(){
+		wget --quiet -O - http://localhost:40080/nginx_status?auto |awk 'NR==3 {print $3}'
+}
 $1
 
